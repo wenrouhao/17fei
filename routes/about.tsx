17b00ce062d45c1a5afc12823fe05ec193099d05
wenrouhao <99107429+wenrouhao@users.noreply.github.com>
2024-04-06 12:46:28 +0800

    Update about.tsx

--- routes/about.tsx
+++ routes/about.tsx
@@ -10,11 +10,11 @@ export default function About() {
                 <div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
                     <div class="text-lg text-gray-500">关于本站</div>
                     <div class="my-4">网站目前只接收客服人工收款</div>
-                    <div>10元开通单月会员</div>
+                    <div>5元开通单月会员</div>
                     <div>
-                        <span class="line-through">99</span>49元开通终身会员
+                        <span class="line-through">99</span>29元开通终身会员
                     </div>
-                    <div>支付后添加微信[ wbot10 ] 1~12小时内响应</div>
+                    <div>支付后添加微信[ Wrh1296520 ] 1~12小时内响应</div>
                     <div class="mt-4">
                         如需其他定制开发， 欢迎联系合作。
                         <a href="/custom" class="underline">如何制作一个相同的网站赚钱</a>
