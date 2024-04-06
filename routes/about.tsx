7844fb7346dd91e60c2ce582996da7cf50416bd8
wenrouhao <99107429+wenrouhao@users.noreply.github.com>
2024-04-06 13:46:37 +0800

    Update about.tsx

--- routes/about.tsx
+++ routes/about.tsx
@@ -14,7 +14,7 @@ export default function About() {
                     <div>
                         <span class="line-through">99</span>29元开通终身会员
                     </div>
-                    <div>支付后添加微信[ Wrh1296520 ] 1~12小时内响应</div>
+                    <div>支付后添加微信[ wrh171 ] 1~12小时内响应</div>
                     <div class="mt-4">
                         如需其他定制开发， 欢迎联系合作。
                         <a href="/custom" class="underline">如何制作一个相同的网站赚钱</a>
